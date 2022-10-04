0ba78b7fa1bd171c2ef70d886a68eb238ba1f73a
Tamas Kralusz <tamas.kralusz@reactron.dev>
2022-10-04 21:14:31 +0300

    customize theme colors and palettes

--- app/src/main/java/com/virtualtag/app/ui/theme/Color.kt
+++ app/src/main/java/com/virtualtag/app/ui/theme/Color.kt
@@ -2,7 +2,14 @@ package com.virtualtag.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+// Primary logo colors
+val Blue500 = Color(0xFF2196F3)
+val Blue700 = Color(0xFF0B5086)
+
+// Light mode background colors
+val Gray200 = Color(0xFFF0F0F0)
+val WhiteBG = Color(0xFFF8F8F8)
+
+// Dark mode background colors
+val BlackBG = Color(0xFF020202)
+val Gray700 = Color(0xFF2E2E2E)
