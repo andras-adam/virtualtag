9be98a7a371219063f1cd79c959f5440a15a1771
Tamas Kralusz <tamas.kralusz@reactron.dev>
2022-10-07 11:33:39 +0300

    update theme colors for dark mode

--- app/src/main/java/com/virtualtag/app/ui/theme/Color.kt
+++ app/src/main/java/com/virtualtag/app/ui/theme/Color.kt
@@ -8,7 +8,7 @@ val Blue700 = Color(0xFF0B5086)
 
 // Light mode background color
 val Gray200 = Color(0xFFF0F0F0)
-val WhiteBG = Color(0xFFF8F8F8)
+val WhiteBG = Color(0xFFFDFDFD)
 
 // Dark mode background colors
 val BlackBG = Color(0xFF020202)
@@ -17,12 +17,12 @@ val Gray700 = Color(0xFF2E2E2E)
 // Background colors for cards
 val cardBackGroundColors = listOf(
     WhiteBG,
-    Color(0xFFFFF0F5),
-    Color(0xFFEBFFFD),
-    Color(0xFFF1FFF1),
-    Color(0xFFFFF7EC),
-    Color(0xFFFFF2EF),
-    Color(0xFFE5F7FF),
-    Color(0xFFFCF1FF),
-    Color(0xFFEBEBEB)
+    Color(0xFFFFD9E5),
+    Color(0xFFC5FFF9),
+    Color(0xFFCAFFCA),
+    Color(0xFFFFEBCF),
+    Color(0xFFFFD6CD),
+    Color(0xFFC9EEFF),
+    Color(0xFFF4CBFF),
+    Color(0xFFFFCDCD)
 )
